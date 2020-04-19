92adda216dfe7734a73ca55f80ae311b8b6ad957
Michael Piercy <michael.piercy@zoho.com>
2020-04-19 16:43:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
-def prime?(number)
+def prime?(int)
   n = 2
-  while n < number
-    return false if number % n == 0
+  while n < int
+    return false if int % n == 0
     n += 1
   end
   true
@@ -10,3 +10,4 @@ end
 
 prime?(2)
 prime?(-1)
+end
